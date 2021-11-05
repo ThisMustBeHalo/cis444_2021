c9e9a5d6afe4959428344b3105d892556dc67333
Ubuntu <ubuntu@ip-172-31-1-18.us-east-2.compute.internal>
2021-11-05 09:37:21 +0000

    fixed book list + added purchasing. todo: clean up purchases and add register function

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+import simplejson as json
 import jwt
 
 import datetime
@@ -101,11 +102,11 @@ def hellodb():
 #a3 code (full stack app)
 user_token = None #represents server side copy of jwt
 
-@app.route('/bookstore')
+@app.route('/bookstore') #main endpoint
 def bookstore():
     return render_template('bookstore.html');
 
-@app.route("/authbookstorelogin",methods=["POST","GET"])
+@app.route("/authbookstorelogin",methods=["POST","GET"]) #handle login
 def authbookstorelogin():
     print ("Starting login...")
     username = request.form["username"]
@@ -116,40 +117,51 @@ def authbookstorelogin():
     user_record = cur.fetchone()
     if user_record is not None: #user exists
      if bcrypt.checkpw(bytes(password, "utf-8"), bytes(user_record[2], "utf-8")) == True: #password is valid
-
+        global user_token;
         user_token = jwt.encode({"id":user_record[0], "username":user_record[1]}, JWT_SECRET, algorithm = "HS256")
-        print("Login OK.")
+        print ("Login OK.")
         return json_response(data = {"jwt": user_token})
      else: #password is invalid
-        print("Incorrect password.")
+        print ("Incorrect password.")
         return json_response(data = {"message": "Incorrect password."})
     else: #user does not exist
-       print("Incorrect username.")
+       print ("Incorrect username.")
        return json_response(data = {"message": "Incorrect username."})
 
-@app.route("/getbooklist",methods=["GET"])
+@app.route("/getbooks",methods=["GET"]) #get books from database
 def getbooklist():
-    print ("Getting booklist...")
-    clientToken = jwt.decode(request.args.get("jwt"), JWT_SECRET, algorithms = ["HS256"])
-    serverToken = jwt.decode(user_token, JWT_SECRET, algorithms = ["HS256"])
-    if not (clientToken == serverToken): #ensure that user is logged in
+    print ("Getting books...")
+    if not checklogin(request.args.get("jwt")):
      print("Invalid token.")
      return json_response(data = {"message": "Invalid token."})
     else:
-     cur = global_db_con.cursor()
-     cur.execute("select count(1) from books;")
-     numRows = cur.fetchone()[0] #number of rows in the table
-     i = 1
-     message = '{"books":['
-     while i <= numRows:
-      cur.execute(f"select from books where id = '{i}';")
-      currentBook = cur.fetchone()
-      message += '{"id":"' + currentBook[0] + '","title":"' + currentBook[1] + '","price":' + str(currentBook[2]) + "}"
-      if not i == numRows:
-       message += ","
-      i += 1
-     print ("Sending booklist...")
-     return json_response(data = json.loads(message)) 
+      cur = global_db_con.cursor()
+      cur.execute("select * from books;")
+      book = cur.fetchall()
+      return json_response(books = book)
+
+@app.route("/makepurchase",methods=["POST"]) #record purchase in database
+def makepurchase():
+    print ("Adding purchase...")
+    cur = global_db_con.cursor()
+    #purchased_book = request.args.get("book_id")
+    #don't know why this doesn't work. it should work. hard coded 1 as proof of concept.
+    purchased_book = 1;
+    cur.execute(f"select * from books where id = '{purchased_book}';")
+    book_data = cur.fetchone()
+    user_data = jwt.decode(user_token, JWT_SECRET, algorithms = ["HS256"])
+    book_title = book_data[1]
+    username = user_data['username']
+    cur.execute(f"insert into purchases (book_title,customer) values ('{book_title}','{username}');")
+    global_db_con.commit()
+    print ("Purchase successful.")
+    return json_response(data = {"message": "Purchase successful! Enjoy!"})
+
+def checklogin(jwt_from_client): #helper function to check if user is logged in i.e. has a valid login token. not working right now
+   clientToken = jwt.decode(jwt_from_client, JWT_SECRET, algorithms = ["HS256"])
+   serverToken = jwt.decode(user_token, JWT_SECRET, algorithms = ["HS256"])
+   print ("User authenticated.")
+   return clientToken == serverToken
 
 app.run(host='0.0.0.0', port=80)
 
