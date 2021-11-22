623b73ceaa97c2b4a71f8f827eec8fa006c6b6c5
ThisMustBeHalo <a.kuo3@hotmail.com>
2021-11-22 13:08:04 -0800

    first attempt to get books info

--- flask_jwt_rest_server/secure_calls/get_books.py
+++ flask_jwt_rest_server/secure_calls/get_books.py
@@ -1,11 +1,15 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
+from db_con import get_db_instance, get_db
+global_db_con = get_db()
 
 from tools.logging import logger
 
 def handle_request():
     logger.debug("Get Books Handle Request")
-
-    return json_response( token = create_token(  g.jwt_data ) , books = {})
+    cur = global_db_con.cursor()
+    cur.execute("select * from books;")
+    books = cur.fetchall()
+    return json_response( token = create_token(  g.jwt_data ) , books)
 
